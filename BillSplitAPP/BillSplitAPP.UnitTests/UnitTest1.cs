f2bc641ef0720b023375ffb9f7c155c2d2386d7e
Sen Wang <senwang@Sens-MacBook-Pro.local>
2018-02-21 21:41:26 -0600

    a backend application for shareing expense

---
+++ BillSplitAPP/BillSplitAPP.UnitTests/UnitTest1.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Splitting;
+using System.Collections;
+using System.Diagnostics;
+
+namespace BillSplitAPP.UnitTests
+{
+    [TestClass]
+    public class HelperTests
+    {
+
+        [TestMethod]
+        public void GetFileName_InputFileNameEndWithTXT_ShouldReturnNewFileNameEndWithOut()
+        {
+            //Arrange
+            string expectedName = "expenses.txt.out";
+            //Act
+
+            var ActualResult = Helper.GetFileName("/Users/senwang/Projects/BillSplitAPP/BillSplitAPP/expenses.txt");
+
+            //Assert
+            Assert.AreEqual(expectedName, ActualResult);
+        }
+
+        [TestMethod]
+        public void RawDataProcessFunction_DataPatternFollowExpensesDotTXTExample_ShouldReturnAListOfString()
+        {
+            //Arrange
+
+            List<string> expectedStringList = new List<string> { "3", "2", "10.00", "20.00", "4", "15.00", "15.01", "3.00", "3.01", "3", "5.00", "9.00", "4.00", "2", "2", "8.00", "6.00", "2", "9.20", "6.75", "0" };
+           
+            //Act
+
+            List<string> ActualResultList = Helper.RawDataProcessFunciton("/Users/senwang/Projects/BillSplitAPP/BillSplitAPP/expenses.txt");
+
+            //Assert
+            CollectionAssert.AreEqual(expectedStringList, ActualResultList, StructuralComparisons.StructuralComparer);
+        }
+
+    }
+
+
+    [TestClass]
+    public class TripTests
+    {
+        
+        [TestMethod]
+        public void LoadTripInfoPersonInfoTripExepnseSummary_DataPatternFollowExpensesDotTXTExample_ShouldLoadAllInfoToListOfTrips()
+        {
+            //Arrange
+
+            List<string> resultList = Helper.RawDataProcessFunciton("/Users/senwang/Projects/BillSplitAPP/BillSplitAPP/expenses.txt");
+
+            //Act
+
+            Trip.LoadTripInfo(resultList);
+            Person.LoadPersonInfo(resultList);
+            Trip.LoadTripExpenseSummary();
+
+            //Assert
+            Assert.AreEqual(2, Helper.ListOfTrips.Count);
+            Assert.AreEqual(1, Helper.ListOfTrips[0].TripID);
+            Assert.AreEqual(2, Helper.ListOfTrips[1].TripID);
+            Assert.AreEqual(3, Helper.ListOfTrips[0].NumPeople);
+            Assert.AreEqual(2, Helper.ListOfTrips[1].NumPeople);
+            Assert.AreEqual(0, Helper.ListOfTrips[0].TripStart);
+            Assert.AreEqual(12, Helper.ListOfTrips[0].TripEnd);
+            Assert.AreEqual(13, Helper.ListOfTrips[1].TripStart);
+            Assert.AreEqual(20, Helper.ListOfTrips[1].TripEnd);
+
+            Assert.AreEqual(84.02.ToString(), Helper.ListOfTrips[0].TripExpenseSum.ToString());
+            Assert.AreEqual(29.95.ToString(), Helper.ListOfTrips[1].TripExpenseSum.ToString());
+            Assert.AreEqual(28.01.ToString(), Helper.ListOfTrips[0].TripExpenseAverage.ToString());
+            Assert.AreEqual(14.98.ToString(), Helper.ListOfTrips[1].TripExpenseAverage.ToString());
+
+
+            Assert.AreEqual(1, Helper.ListOfTrips[0].IndividualData[0].PersonID);
+            Assert.AreEqual(2, Helper.ListOfTrips[0].IndividualData[1].PersonID);
+            Assert.AreEqual(3, Helper.ListOfTrips[0].IndividualData[2].PersonID);
+            Assert.AreEqual(1, Helper.ListOfTrips[1].IndividualData[0].PersonID);
+            Assert.AreEqual(2, Helper.ListOfTrips[1].IndividualData[1].PersonID);
+
+            Assert.AreEqual(2, Helper.ListOfTrips[0].IndividualData[0].NumExpenses);
+            Assert.AreEqual(4, Helper.ListOfTrips[0].IndividualData[1].NumExpenses);
+            Assert.AreEqual(3, Helper.ListOfTrips[0].IndividualData[2].NumExpenses);
+            Assert.AreEqual(2, Helper.ListOfTrips[1].IndividualData[0].NumExpenses);
+            Assert.AreEqual(2, Helper.ListOfTrips[1].IndividualData[1].NumExpenses);
+
+            Assert.AreEqual(2, Helper.ListOfTrips[0].IndividualData[0].PersonExpenseStart);
+            Assert.AreEqual(3, Helper.ListOfTrips[0].IndividualData[0].PersonExpenseEnd);
+            Assert.AreEqual(5, Helper.ListOfTrips[0].IndividualData[1].PersonExpenseStart);
+            Assert.AreEqual(8, Helper.ListOfTrips[0].IndividualData[1].PersonExpenseEnd);
+            Assert.AreEqual(10, Helper.ListOfTrips[0].IndividualData[2].PersonExpenseStart);
+            Assert.AreEqual(12, Helper.ListOfTrips[0].IndividualData[2].PersonExpenseEnd);
+            Assert.AreEqual(15, Helper.ListOfTrips[1].IndividualData[0].PersonExpenseStart);
+            Assert.AreEqual(16, Helper.ListOfTrips[1].IndividualData[0].PersonExpenseEnd);
+            Assert.AreEqual(18, Helper.ListOfTrips[1].IndividualData[1].PersonExpenseStart);
+            Assert.AreEqual(19, Helper.ListOfTrips[1].IndividualData[1].PersonExpenseEnd);
+
+            Assert.AreEqual((float)10.00, Helper.ListOfTrips[0].IndividualData[0].ItemizedExpense[0]);
+            Assert.AreEqual((float)20.00, Helper.ListOfTrips[0].IndividualData[0].ItemizedExpense[1]);
+            Assert.AreEqual((float)15.00, Helper.ListOfTrips[0].IndividualData[1].ItemizedExpense[0]);
+            Assert.AreEqual((float)15.01, Helper.ListOfTrips[0].IndividualData[1].ItemizedExpense[1]);
+            Assert.AreEqual((float)3.00, Helper.ListOfTrips[0].IndividualData[1].ItemizedExpense[2]);
+            Assert.AreEqual((float)3.01, Helper.ListOfTrips[0].IndividualData[1].ItemizedExpense[3]);
+            Assert.AreEqual((float)5.00, Helper.ListOfTrips[0].IndividualData[2].ItemizedExpense[0]);
+            Assert.AreEqual((float)9.00, Helper.ListOfTrips[0].IndividualData[2].ItemizedExpense[1]);
+            Assert.AreEqual((float)4.00, Helper.ListOfTrips[0].IndividualData[2].ItemizedExpense[2]);
+            Assert.AreEqual((float)8.00, Helper.ListOfTrips[1].IndividualData[0].ItemizedExpense[0]);
+            Assert.AreEqual((float)6.00, Helper.ListOfTrips[1].IndividualData[0].ItemizedExpense[1]);
+            Assert.AreEqual((float)9.20, Helper.ListOfTrips[1].IndividualData[1].ItemizedExpense[0]);
+            Assert.AreEqual((float)6.75, Helper.ListOfTrips[1].IndividualData[1].ItemizedExpense[1]);
+
+            Assert.AreEqual((float)30.00, Helper.ListOfTrips[0].IndividualData[0].PersonSum);
+            Assert.AreEqual((float)36.02, Helper.ListOfTrips[0].IndividualData[1].PersonSum);
+            Assert.AreEqual((float)18.00, Helper.ListOfTrips[0].IndividualData[2].PersonSum);
+            Assert.AreEqual((float)14.00, Helper.ListOfTrips[1].IndividualData[0].PersonSum);
+            Assert.AreEqual((float)15.95, Helper.ListOfTrips[1].IndividualData[1].PersonSum);
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
---
+++ BillSplitAPP/BillSplitAPP/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Splitting
+{
+
+
+    public static class Helper
+    {
+
+        // create a global list to contain the trips from the TXT file
+        public static List<Trip> ListOfTrips = new List<Trip>();
+
+        public static string GetFileName(String pathToTxtFile)
+        {
+            String OutputFileName = Path.GetFileName(pathToTxtFile) + ".out";
+            return OutputFileName;
+        }
+
+        public static List<string> RawDataProcessFunciton(String pathToTxtFile)
+        {
+            int LineCounter = 0;
+            String LineValue;
+            StreamReader sr = new StreamReader(pathToTxtFile);
+            List<String> CampInfoSumArray = new List<String>();
+            LineValue = sr.ReadLine();
+
+            while (LineValue != null)
+            {
+                if (LineValue.Length != 0)
+                {
+                    CampInfoSumArray.Add(LineValue);
+                    LineCounter++;
+                }
+                LineValue = sr.ReadLine();
+            }
+            sr.Close();
+
+            return CampInfoSumArray;
+        }
+
+    }
+
+    public class Trip
+    {
+        public int TripID;
+        public int TripEnd;
+        public int NumPeople;
+        public int TripStart;
+        public float TripExpenseSum;
+        public float TripExpenseAverage;
+        public List<Person> IndividualData = new List<Person>();
+
+
+        public static void LoadTripInfo(List<string> CampInfoSumArray)
+        {
+            int TripCounter = 0;
+            for (int i = 0; i < CampInfoSumArray.Count() - 1; i++)
+            {
+                if (CampInfoSumArray[i].Length < 3 && CampInfoSumArray[i + 1].Length < 3)
+                {
+                    Trip Trip = new Trip();
+                    Helper.ListOfTrips.Add(Trip);
+                    Trip.TripID = TripCounter + 1;
+                    Trip.TripStart = i;
+                    Trip.NumPeople = Int32.Parse(CampInfoSumArray[i]);
+                    TripCounter++;
+                }
+
+            }
+
+            //  load end point of each trip
+
+            for (int i = 0; i < Helper.ListOfTrips.Count() - 1; i++)
+            {
+                Helper.ListOfTrips[i].TripEnd = Helper.ListOfTrips[i + 1].TripStart - 1;
+            }
+            Helper.ListOfTrips[Helper.ListOfTrips.Count() - 1].TripEnd = CampInfoSumArray.Count() - 1;
+        }
+
+
+
+        public static void LoadTripExpenseSummary()
+        {
+            for (int i = 0; i < Helper.ListOfTrips.Count(); i++)
+            {
+                float TempTripSum = 0;
+                for (int j = 0; j < Helper.ListOfTrips[i].IndividualData.Count(); j++)
+                {
+                    TempTripSum += Helper.ListOfTrips[i].IndividualData[j].PersonSum;
+                }
+                Helper.ListOfTrips[i].TripExpenseSum = TempTripSum;
+
+                float temporaryNumber = Helper.ListOfTrips[i].TripExpenseSum / Helper.ListOfTrips[i].NumPeople;
+                Helper.ListOfTrips[i].TripExpenseAverage = (float)(Math.Round((double)temporaryNumber, 2));
+            }
+        }
+
+    }
+
+
+    public class Person
+    {
+        public int PersonID;
+        public int NumExpenses;
+        public float PersonSum;
+        public int PersonExpenseEnd;
+        public int PersonExpenseStart;
+        public List<float> ItemizedExpense = new List<float>();
+
+
+
+        public static void LoadPersonInfo(List<string> CampInfoSumArray)
+        {
+            for (int i = 0; i < Helper.ListOfTrips.Count(); i++)
+            {
+                int NumPersonCounter = 0;
+                for (int j = Helper.ListOfTrips[i].TripStart; j <= Helper.ListOfTrips[i].TripEnd - 1; j++)
+                {
+
+                    if (CampInfoSumArray[j].Length < 3 && CampInfoSumArray[j + 1].Length >= 3)
+                    {
+                        Person Person = new Person();
+
+                        Person.PersonID = NumPersonCounter + 1;
+                        Person.NumExpenses = Int32.Parse(CampInfoSumArray[j]);
+                        Person.PersonExpenseStart = j + 1;
+                        Person.PersonExpenseEnd = Person.NumExpenses + Person.PersonExpenseStart - 1;
+                        for (int n = Person.PersonExpenseStart; n <= Person.PersonExpenseEnd; n++)
+                        {
+                            Person.ItemizedExpense.Add(float.Parse(CampInfoSumArray[n]));
+                        }
+                        Person.PersonSum = Person.ItemizedExpense.Sum();
+
+                        NumPersonCounter++;
+
+                        Helper.ListOfTrips[i].IndividualData.Add(Person);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+   
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string NewFileName = Helper.GetFileName("/Users/senwang/Projects/BillSplitAPP/BillSplitAPP/expenses.txt");
+                List<string> resultList = Helper.RawDataProcessFunciton("/Users/senwang/Projects/BillSplitAPP/BillSplitAPP/expenses.txt");
+                Trip.LoadTripInfo(resultList);
+                Person.LoadPersonInfo(resultList);
+                Trip.LoadTripExpenseSummary();
+
+
+                String fmt = "$#,##0.00;($#,##0.00)";
+                using (StreamWriter writer = new StreamWriter(NewFileName))
+                {
+                    for (int i = 0; i < Helper.ListOfTrips.Count(); i++)
+                    {
+                        for (int j = 0; j < Helper.ListOfTrips[i].IndividualData.Count(); j++)
+                        {
+                            writer.WriteLine((Helper.ListOfTrips[i].TripExpenseAverage - Helper.ListOfTrips[i].IndividualData[j].PersonSum).ToString(fmt));
+                        }
+                        writer.WriteLine();
+                    }
+                }
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
+    }
+}
